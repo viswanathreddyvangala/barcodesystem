e8d11ff37fc81843e29bd00325ec5aa4a3116a66
viswanathreddyvangala <v.viswa000@gmail.com>
2025-07-20 08:21:31 +0000

    Fixed changes

--- frontend-app/inventag-admin/src/main.tsx
+++ frontend-app/inventag-admin/src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
 import ItemDetail from './itemDetail';
-import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
